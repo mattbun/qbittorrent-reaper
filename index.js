28741f8631ce8fb4e81143d78fbd092367c64784
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2021-01-30 17:15:56 -0500

    Move env var reading to another file

--- index.js
+++ index.js
@@ -2,24 +2,20 @@ const Promise = require('bluebird');
 const api = require('qbittorrent-api-v2');
 const parseDuration = require('parse-duration');
 
-const {
-  QBITTORRENT_HOST,
-  QBITTORRENT_USER,
-  QBITTORRENT_PASSWORD,
-  MAX_TORRENT_AGE,
-  DRY_RUN,
-} = process.env;
+const readConfig = require('./lib/config');
 
 const MILLISECONDS_IN_ONE_SECOND = 1000;
 
+const config = readConfig();
+
 (async () => {
-  console.info(`Connecting to qbittorrent at ${QBITTORRENT_HOST}...`);
-  const qbittorrent = await api.connect(QBITTORRENT_HOST, QBITTORRENT_USER, QBITTORRENT_PASSWORD);
+  console.info(`Connecting to qbittorrent at ${config.host}...`);
+  const qbittorrent = await api.connect(config.host, config.user, config.password);
 
   console.info('Connected! Getting torrents...');
   const torrents = await qbittorrent.torrents();
 
-  const earliestTorrentDate = new Date((new Date().getTime()) - parseDuration(MAX_TORRENT_AGE));
+  const earliestTorrentDate = new Date((new Date().getTime()) - parseDuration(config.maxAge));
   console.log(`Looking for torrents added before ${earliestTorrentDate}`);
   const oldTorrents = torrents
     .map(torrent => ({
@@ -29,9 +25,14 @@ const MILLISECONDS_IN_ONE_SECOND = 1000;
     }))
     .filter(torrent => torrent.added_on < earliestTorrentDate);
 
-
   await Promise.each(oldTorrents, torrent => {
     process.stdout.write(`Removing ${torrent.name}...`);
+
+    if (!config.isDryRun) {
+      // TODO actually delete the torrent
+      process.stdout.write('TODO...');
+    }
+
     process.stdout.write(' Done\n');
   });
 })().catch(error => console.error(error));
